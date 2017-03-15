ad1c98d07a0edff4069ba27439980f229c8f271b
bastien <dearaujo.bastien@gmail.com>
2017-03-15 10:29:40 +0100

    ajout piece.c avec fonction conversion et type enum couleur et pieces

---
+++ application/version1/the_sources/echiquier.c
@@ -0,0 +1,8 @@
+#include "main.c"
+#include "pieces.c"
+
+
+typedef struct case_t {
+  int couleur;
+  int piece;
+} case_t ;
---
+++ application/version1/the_sources/pieces.c
@@ -0,0 +1,66 @@
+typedef enum couleur_t {
+	BLANC, NOIR;
+} couleur_t ;
+
+typedef enum piece_t {
+	K, Q, B, N, R, P, VIDE, ETOILE; // VIDE=' ' ETOILE= '*' 
+} piece_t ;
+
+
+piece_t piece_de_lettre (char piece){
+	switch (piece){
+		case 'K':
+			return(K);
+			break;
+		case 'Q':
+			return(Q);
+			break;
+		case 'B':
+			return(B);
+			break;
+		case 'N':
+			return(N);
+			break;
+		case 'R':
+			return(R);
+			break;
+		case 'P':
+			return(P);
+			break;
+		case ' ':
+			return(VIDE);
+			break;
+		default:
+			return(ETOILE);
+		  	break;
+	}
+}
+
+char lettre_de_piece (piece_t piece){
+	switch (piece){
+		case K:
+			return('K');
+			break;
+		case Q:
+			return('Q');
+			break;
+		case B:
+			return('B');
+			break;
+		case N:
+			return('N');
+			break;
+		case R:
+			return('R');
+			break;
+		case P:
+			return('P');
+			break;
+		case VIDE:
+			return(' ');
+			break;
+		default:
+			return('*');
+		  	break;
+	}
+}
